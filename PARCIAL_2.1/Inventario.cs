dbdfbe7b1ae1ac4e074deba5791a1e7e913301c8
Nayeli-Urrutia <160196354+Nayeli-Urrutia@users.noreply.github.com>
2024-04-24 20:58:51 -0600

    Agregar archivos de proyecto.

---
+++ PARCIAL_2.1/Inventario.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARCIAL_2._1
+{
+    internal class Inventario
+    {
+        private string[] productos;
+        private int[] cantidades;
+
+        // Indexador para acceder a la cantidad de un producto por su nombre
+        public int this[string producto]
+        {
+            get
+            {
+                int index = Array.IndexOf(productos, producto);
+                if (index != -1)
+                {
+                    return cantidades[index];
+                }
+                else
+                {
+                    return -1; 
+                }
+            }
+            set
+            {
+                int index = Array.IndexOf(productos, producto);
+                if (index != -1)
+                {
+                    cantidades[index] = value;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"El producto '{producto}' no está en el inventario.");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        // Propiedad para obtener la cantidad total de productos en el inventario
+        public int CantidadTotal
+        {
+            get
+            {
+                int total = 0;
+                foreach (int cantidad in cantidades)
+                {
+                    total += cantidad;
+                }
+                return total;
+            }
+        }
+
+        // Constructor
+        public Inventario(int tamaño)
+        {
+            productos = new string[tamaño];
+            cantidades = new int[tamaño];
+        }
+
+        // Método para agregar un producto al inventario
+        public void AgregarProducto(string producto, int cantidad)
+        {
+            for (int i = 0; i < productos.Length; i++)
+            {
+                if (productos[i] == null)
+                {
+                    productos[i] = producto;
+                    cantidades[i] = cantidad;
+                    return;
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("El inventario está lleno, no se puede agregar más productos.");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ PARCIAL_2.1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARCIAL_2._1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Inventario inventario = new Inventario(5);
+
+        
+            inventario.AgregarProducto("Camiseta", 10);
+            inventario.AgregarProducto("Pantalón", 5);
+            inventario.AgregarProducto("Zapatos", 8);
+
+            // Acceder a la cantidad de productos utilizando el indexador
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("**************************************************");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Cantidad de productos en el inventario:");
+            Console.ResetColor();
+            Console.WriteLine($"- Camisetas: {inventario["Camiseta"]}");
+            Console.WriteLine($"- Pantalones: {inventario["Pantalón"]}");
+            Console.WriteLine($"- Zapatos: {inventario["Zapatos"]}");
+            
+
+            // Modificar la cantidad de productos utilizando el indexador
+            inventario["Camiseta"] = 10;
+            inventario["Pantalón"] =  5;
+
+
+
+            // Acceder a la cantidad total de productos utilizando la propiedad
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Cantidad total de productos en el inventario: {inventario.CantidadTotal}");
+            Console.ResetColor();
+           
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("**************************************************");
+            Console.ResetColor();
+
+
+            Console.ReadKey();
+        }
+    }
+        
+ }
+
+        
+    
+
